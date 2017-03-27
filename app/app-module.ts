1263ba1a8bd34156c3b7ebbaccc4f15299055acf
Dmitri Maksimov <dmitri.maksimov@bytelife.com>
2017-03-28 01:21:17 +0300

    2nd module.

--- app/app-module.ts
+++ app/app-module.ts
@@ -3,11 +3,13 @@ import {BrowserModule} from "@angular/platform-browser";
 import {EventsAppComponent} from "./events-app-component";
 import {EventListComponent} from "./events/events-list.component";
 import {EventThumbnailComponent} from "./events/event-thumbnail.component";
+import {NavBarComponent} from "./nav/navbar.component";
 
 @NgModule({
     imports: [BrowserModule],
     declarations: [
         EventsAppComponent,
+        NavBarComponent,
         EventListComponent,
         EventThumbnailComponent
     ],
--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -1,15 +1,12 @@
-import {Component, EventEmitter, Input, Output} from "@angular/core";
+import {Component, Input} from "@angular/core";
 
 @Component({
     selector: 'event-thumbnail',
-    templateUrl: 'app/events/event-thumbnail.component.html'
+    templateUrl: 'app/events/event-thumbnail.component.html',
+    styles: [
+        '.well div { color: #bbb; }'
+    ]
 })
 export class EventThumbnailComponent {
     @Input() event: any
-    @Output() eventClick = new EventEmitter
-
-    handleClickMe() {
-        console.log("clicked");
-        this.eventClick.emit(this.event.name);
-    }
 }
