fd9bb3519fc962cf197504c713621e4f4f4303da
Dmitri Maksimov <dmitri.maksimov@bytelife.com>
2017-04-05 17:05:24 +0300

    Fixed routing between event details not working.

--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {EventService} from '../shared/event.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Params} from '@angular/router';
 import {IEvent, ISession} from '../shared/event.model';
 
 @Component({
@@ -28,11 +28,13 @@ export class EventDetailsComponent implements OnInit {
   sortBy: string = 'votes';
 
   constructor(private eventService: EventService,
-              private route: ActivatedRoute) {
-  }
+              private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
+    this.route.params.subscribe((params: Params) => {
+      this.event = this.eventService.getEvent(+params['id']);
+      this.addMode = false;
+    });
   }
 
   addSession(): void {
